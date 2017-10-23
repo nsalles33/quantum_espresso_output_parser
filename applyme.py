d8fa290943c60008614bf8fa9e044073f68a0cf2
Ruggero Lot <ruggero90@gmail.com>
2017-10-23 09:43:50 +0200

    add # of processed file info

--- applyme.py
+++ applyme.py
@@ -6,14 +6,17 @@ from data_magic import *
 logging.basicConfig(filename='./parse.log', level=logging.INFO)
 
 simulationsData = []
-mypath = './new_test'
+mypath = './new_tests'
 files = [os.path.join(mypath, f) for f in os.listdir(mypath) if
          os.path.isfile(os.path.join(mypath, f)) if f.startswith('output')]
 
 summa = {}
-for x in files:
+for i, x in enumerate(files):
+    print('{}/{}'.format(i, len(files)))
     logging.info('--------------' + x + '----------------')
     summa.update(file_parser(x))
 
-with open('v0.2.1.json', 'w') as f:
+with open('v0.2.2.json', 'w') as f:
     json.dump(summa, f)
+
+logging.info('# of simulations: {}'.format(len(summa)))
